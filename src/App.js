23e0116cf81c63c9bcdcacb46a137521c8f7256b
mbdx4srt <35216966+mbdx4srt@users.noreply.github.com>
2021-10-26 23:36:23 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import ListList from "./components/ListList"
+
+
+function App() {
+  return (
+    <div className="listmaker">
+
+        <ListList />
+
+    </div>
+  );
+}
+
+export default App;
